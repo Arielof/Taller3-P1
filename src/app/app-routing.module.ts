e7b7eb61a042b6ad9609c72d61bab68fa3278d30
Arielof <aop.floril@yavirac.edu.ec>
2022-11-28 03:21:14 -0600

    taller tres

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { StaticRoutingModule } from './static/static-routing.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+import { NotFoundRoutingModule } from './not-found/not-found-routing.module';
+//import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+
+ const routes:Routes = [       
+    {path:'', redirectTo:'dashboard', pathMatch:'full'},    
+    {path:'**', component:NotFoundComponent}    
+ ];
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    NotFoundRoutingModule,
+    AuthRoutingModule,
+    StaticRoutingModule    
+  ],
+  exports:[RouterModule],
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { AuthModule } from './auth/auth.module';
+import { NotFoundModule } from './not-found/not-found.module';
+import { StaticModule } from './static/static.module';
+import { PagesComponent } from './pages/pages.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,            
+    PagesComponent    
+            
+  ],
+  imports: [
+    BrowserModule,
+    StaticModule,    
+    AppRoutingModule,
+    NotFoundModule,
+    AuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
